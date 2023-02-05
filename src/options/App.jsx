9c7c83bcc7347624bf3af723f93074f0e6eb4bae
Akshit Bansal <akshit.bansal755@gmail.com>
2023-02-05 15:26:09 +0530

    Added Icon along with title

--- src/options/App.jsx
+++ src/options/App.jsx
@@ -1,7 +1,7 @@
 import History from "./components/History";
 import { Route, Switch } from "react-router-dom";
 import { Fragment, useEffect, lazy, Suspense } from "react";
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 const Favourites = lazy(() => import("./components/Favourites"));
 import Navbar from "./components/common/Navbar";
 import { useQueryClient } from "react-query";
@@ -9,6 +9,7 @@ import { fetch as fetchFavourites } from "./services/Favourites";
 import LoadingFallback from "./components/common/LoadingFallback";
 const Sessions = lazy(() => import("./components/Sessions"));
 const Settings = lazy(() => import("./components/Settings"));
+import icon from "../../public/assets/icon-128.png";
 
 const App = () => {
 	const queryClient = useQueryClient();
@@ -21,7 +22,17 @@ const App = () => {
 		<Fragment>
 			<Navbar />
 			<Container>
-				<Typography variant="h2" component="h1" align="center">
+				<Typography
+					variant="h2"
+					component="h1"
+					align="center"
+					display="flex"
+					justifyContent="center"
+					alignItems="center"
+				>
+					<Box display="flex" marginRight={1}>
+						<img src={icon} width={64} height={64} />
+					</Box>
 					Private History
 				</Typography>
 				<Switch>
