143fca56d4f6a2392b98ccd9bf97c58833d8bf55
Akshit Bansal <akshit.bansal755@gmail.com>
2023-03-31 15:16:38 +0530

    Unexpectedly closed sessions now recovered

--- src/background/sessions.js
+++ src/background/sessions.js
@@ -15,3 +15,12 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 chrome.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
 	await cache.sessions.removeTab(tabId, removeInfo);
 });
+
+chrome.runtime.onStartup.addListener(async () => {
+	const sessions = await cache.sessions.getCache();
+	for (const sessionId in sessions) {
+		const session = sessions[sessionId];
+		if (session.timestamp) continue;
+		await cache.sessions.closeSession(sessionId);
+	}
+});
